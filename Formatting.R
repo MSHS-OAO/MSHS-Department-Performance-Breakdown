3b3d0a111b13bc5260179f22b8be2f82f48f42d0
Weber <anjelica.weber@mssm.edu>
2022-04-26 12:47:51 -0400

    Testing function on df and list

--- Formatting.R
+++ Formatting.R
@@ -5,9 +5,7 @@ percent_formatting <- function(df, col_name){
     mutate_at(vars(contains(col_name)), ~ round(.x, digits = 2)) %>%
     mutate_at(vars(contains(col_name)), ~ paste0(.x, "%"))
 }
-test1 <- percent_formatting(comparison_calculations,c("%", "Index"))
-percent_formatting <- function(df, col_name){
-  df <- df %>%
-    mutate_at(vars(contains(col_name)), ~ round(.x, digits = 2)) %>%
-    mutate_at(vars(contains(col_name)), ~ paste0(.x, "%"))
-}
+
+test_df <- percent_formatting(comparison_calculations, c("%", "Index"))
+
+test_list <- lapply(roll, function(x) percent_formatting(x, c("%", "Index")))
