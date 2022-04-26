03eb2b44c7d97e9e17bd39d0bbd13ae8a29d060b
Weber <anjelica.weber@mssm.edu>
2022-04-26 14:47:17 -0400

    Adding in argument for user to chose columns by key words or exact names

--- Formatting.R
+++ Formatting.R
@@ -1,11 +1,17 @@
 
 # Creating Custom Functions ------------------------------------------------
-percent_formatting <- function(df, col_name){
-  df <- df %>%
-    mutate_at(vars(contains(col_name)), ~ round(.x, digits = 2)) %>%
-    mutate_at(vars(contains(col_name)), ~ paste0(.x, "%"))
+percent_formatting <- function(df, col_name, matches = F){
+  if(matches == F){
+    df <- df %>%
+      mutate_at(vars(contains(col_name)), ~ round(.x, digits = 2)) %>%
+      mutate_at(vars(contains(col_name)), ~ paste0(.x, "%")) 
+  }else if (matches == T){
+    df <- df %>%
+      mutate_at(vars(ends_with(col_name)), ~ round(.x, digits = 2)) %>%
+      mutate_at(vars(ends_with(col_name)), ~ paste0(.x, "%"))   
+  }
 }
 
-test_df <- percent_formatting(comparison_calculations, c("%", "Index"))
+test_df <- percent_formatting(comparison_calculations, c("%", "Index"), matches = F)
 
 test_list <- lapply(roll, function(x) percent_formatting(x, c("%", "Index")))
